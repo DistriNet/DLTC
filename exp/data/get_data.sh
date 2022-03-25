4414abccb8913a3d6313b22d83d798196b703046
vera.rimmer@kuleuven.be <vera.rimmer@kuleuven.be>
2022-03-25 01:31:45 +0100

    Update belnet link and add results files

--- exp/data/get_data.sh
+++ exp/data/get_data.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-curl 'https://filesender.belnet.be/download.php?token=4f5b2172-f6e3-4d3d-bd8a-1ffda8ee4d16&files_ids=1039431%2C1039432%2C1039433%2C1039434%2C1039435%2C1039436' --output all_data.tar
+curl 'https://filesender.belnet.be/download.php?token=57b4bc25-bde4-4fd1-be15-bf0a6e4ee797&files_ids=1056900%2C1056901' > belnet_file.zip
+unzip belnet_file.zip
 tar -xvf all_data.tar
+tar -xvf example_results.tar -C ../results/
