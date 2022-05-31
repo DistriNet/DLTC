72499bd3990b4c25c70d3fb0aa0715472b2ea552
verarim <vera.rimmer@kuleuven.be>
2022-05-31 16:18:02 +0200

    Update get_data.sh

--- exp/data/get_data.sh
+++ exp/data/get_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl 'https://filesender.belnet.be/download.php?token=4f5b2172-f6e3-4d3d-bd8a-1ffda8ee4d16&files_ids=1039431%2C1039432%2C1039433%2C1039434%2C1039435%2C1039436' --output all_data.tar
+curl 'https://filesender.belnet.be/download.php?token=56b46a01-7d0b-4dfe-b939-ab406b77bd45&files_ids=1190979%2C1190980' --output all_data.tar
 tar -xvf all_data.tar
